0d5e03c182a5c6e2d59146f85f5eed12f475292c
YourtionGuo <yourtion@gmail.com>
2019-05-11 16:41:03 +0800

    feat(eureka): 单机版本Eureka

---
+++ ad-eureka/src/main/java/com/yourtion/springcloud/ad/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.yourtion.springcloud.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author yourtion
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
