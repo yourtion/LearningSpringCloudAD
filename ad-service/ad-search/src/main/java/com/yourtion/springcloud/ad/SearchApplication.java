9186b2a0b720c5a98e1df5798cd35de8edcfa92f
YourtionGuo <yourtion@gmail.com>
2019-05-15 21:30:58 +0800

    feat(ad-search): 初始化ad-search项目

---
+++ ad-service/ad-search/src/main/java/com/yourtion/springcloud/ad/SearchApplication.java
@@ -0,0 +1,27 @@
+package com.yourtion.springcloud.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author yourtion
+ */
+@EnableFeignClients
+@EnableEurekaClient
+@EnableHystrix
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+@SpringBootApplication
+public class SearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApplication.class, args);
+    }
+}
