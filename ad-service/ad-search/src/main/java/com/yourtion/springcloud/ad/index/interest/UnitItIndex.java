d7dfa85deac160c324e0e78e7107a2e228200612
YourtionGuo <yourtion@gmail.com>
2019-05-18 21:33:40 +0800

    feat(ad-search): 添加Interest索引方法

---
+++ ad-service/ad-search/src/main/java/com/yourtion/springcloud/ad/index/interest/UnitItIndex.java
@@ -0,0 +1,106 @@
+package com.yourtion.springcloud.ad.index.interest;
+
+import com.yourtion.springcloud.ad.index.IndexAware;
+import com.yourtion.springcloud.ad.utils.CommonUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+
+/**
+ * @author yourtion
+ */
+@Slf4j
+@Component
+public class UnitItIndex implements IndexAware<String, Set<Long>> {
+
+    /**
+     * itTag, adUnitId set
+     */
+    private static Map<String, Set<Long>> itUnitMap;
+
+    /**
+     * unitId, itTag set
+     */
+    private static Map<Long, Set<String>> unitItMap;
+
+    static {
+        itUnitMap = new ConcurrentHashMap<>();
+        unitItMap = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public Set<Long> get(String key) {
+        return itUnitMap.get(key);
+    }
+
+    @Override
+    public void add(String key, Set<Long> value) {
+
+        log.trace("UnitItIndex, before add: {}", unitItMap);
+
+        Set<Long> unitIds = CommonUtils.getOrCreate(
+                key, itUnitMap,
+                ConcurrentSkipListSet::new
+        );
+        unitIds.addAll(value);
+
+        for (Long unitId : value) {
+
+            Set<String> its = CommonUtils.getOrCreate(
+                    unitId, unitItMap,
+                    ConcurrentSkipListSet::new
+            );
+            its.add(key);
+        }
+
+        log.trace("UnitItIndex, after add: {}", unitItMap);
+    }
+
+    @Override
+    public void update(String key, Set<Long> value) {
+
+        log.error("it index can not support update");
+    }
+
+    @Override
+    public void delete(String key, Set<Long> value) {
+
+        log.trace("UnitItIndex, before delete: {}", unitItMap);
+
+        Set<Long> unitIds = CommonUtils.getOrCreate(
+                key, itUnitMap,
+                ConcurrentSkipListSet::new
+        );
+        unitIds.removeAll(value);
+
+        for (Long unitId : value) {
+            Set<String> itTagSet = CommonUtils.getOrCreate(
+                    unitId, unitItMap,
+                    ConcurrentSkipListSet::new
+            );
+            itTagSet.remove(key);
+        }
+
+        log.trace("UnitItIndex, after delete: {}", unitItMap);
+    }
+
+    public boolean match(Long unitId, List<String> itTags) {
+
+        if (unitItMap.containsKey(unitId)
+                && CollectionUtils.isNotEmpty(unitItMap.get(unitId))) {
+
+            Set<String> unitKeywords = unitItMap.get(unitId);
+
+            return CollectionUtils.isSubCollection(itTags, unitKeywords);
+        }
+
+        return false;
+    }
+}
---
+++ ad-service/ad-search/src/main/java/com/yourtion/springcloud/ad/index/interest/UnitItObject.java
@@ -0,0 +1,18 @@
+package com.yourtion.springcloud.ad.index.interest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+/**
+ * @author yourtion
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UnitItObject {
+
+    private Long unitId;
+    private String itTag;
+}
