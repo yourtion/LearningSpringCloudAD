1302d29f47114d04cd65aaa092ebb73caedab302
YourtionGuo <yourtion@gmail.com>
2019-06-27 10:09:39 +0800

    feat(ad-common): 添加导出定义

---
+++ ad-service/ad-common/src/main/java/com/yourtion/springcloud/ad/dump/DConstant.java
@@ -0,0 +1,22 @@
+package com.yourtion.springcloud.ad.dump;
+
+/**
+ * 导出文件定义
+ *
+ * @author yourtion
+ */
+public class DConstant {
+
+    // 数据目录
+    public static final String DATA_ROOT_DIR = "/tmp/ad";
+
+    // 表数据文件存储名称
+    public static final String AD_PLAN = "ad_plan.data";
+    public static final String AD_UNIT = "ad_unit.data";
+    public static final String AD_CREATIVE = "ad_creative.data";
+    public static final String AD_CREATIVE_UNIT = "ad_creative_unit.data";
+    public static final String AD_UNIT_IT = "ad_unit_it.data";
+    public static final String AD_UNIT_DISTRICT = "ad_unit_district.data";
+    public static final String AD_UNIT_KETWORD = "ad_unit_ketword.data";
+
+}
