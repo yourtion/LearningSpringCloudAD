a0f2d5a7cebdb8334a83a155fb54fd15452e23c4
YourtionGuo <yourtion@gmail.com>
2019-06-29 10:40:00 +0800

    feat(db): 更新数据SQL

--- db.sql
+++ db.sql
@@ -120,3 +120,103 @@ CREATE TABLE `ad_unit_district`
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT ='推广单元地域 Feature';
+
+
+--  插入数据
+
+INSERT INTO `ad_user`
+VALUES (15, 'qinyi', 'B2E56F2420D73FEC125D2D51641C5713', 1, '2018-11-19 20:29:01', '2018-11-19 20:29:01');
+INSERT INTO `ad_creative`
+VALUES (10, '第一个创意', 1, 1, 720, 1080, 1024, 0, 1, 15, 'https://www.imooc.com', '2018-11-19 21:31:31',
+        '2018-11-19 21:31:31');
+INSERT INTO `ad_plan`
+VALUES (10, 15, '推广计划名称', 1, '2018-11-28 00:00:00', '2019-11-20 00:00:00', '2018-11-19 20:42:27',
+        '2018-11-19 20:57:12');
+INSERT INTO `ad_unit`
+VALUES (10, 10, '第一个推广单元', 1, 1, 10000000, '2018-11-20 11:43:26', '2018-11-20 11:43:26'),
+       (12, 10, '第二个推广单元', 1, 1, 15000000, '2018-01-01 00:00:00', '2018-01-01 00:00:00');
+INSERT INTO `ad_unit_district`
+VALUES (10, 10, '安徽省', '淮北市'),
+       (11, 10, '安徽省', '宿州市'),
+       (12, 10, '安徽省', '合肥市'),
+       (14, 10, '辽宁省', '大连市');
+INSERT INTO `ad_unit_it`
+VALUES (10, 10, '台球'),
+       (11, 10, '游泳'),
+       (12, 10, '乒乓球');
+INSERT INTO `ad_unit_keyword`
+VALUES (10, 10, '宝马'),
+       (11, 10, '奥迪'),
+       (12, 10, '大众');
+INSERT INTO `creative_unit`
+VALUES (10, 10, 10);
+
+
+-- 更新数据
+
+-- 更新, 插入, 删除 ad_plan
+update ad_plan
+set start_date = '2018-11-30 00:00:00'
+where id = 10;
+INSERT INTO `ad_plan` (`user_id`, `plan_name`, `plan_status`, `start_date`, `end_date`, `create_time`, `update_time`)
+VALUES (10, '第二个推广计划', 1, '2018-01-01 00:00:00', '2019-12-01 00:00:00', '2018-01-01 00:00:00', '2018-01-01 00:00:00');
+delete
+from ad_plan
+where id = 12;
+
+
+-- 更新, 插入, 删除 ad_creative
+update ad_creative
+set url = 'https://www.imooc.com';
+INSERT INTO `ad_creative` (`name`, `type`, `material_type`, `height`, `width`, `size`, `duration`, `audit_status`,
+                           `user_id`, `url`, `create_time`, `update_time`)
+VALUES ('第二个创意', 1, 1, 720, 1080, 1024, 0, 1, 10, 'www.imooc.com', '2018-01-01 00:00:00', '2018-01-01 00:00:00');
+delete
+from ad_creative
+where id = 13;
+
+
+-- 更新, 插入, 删除 ad_unit
+update ad_unit
+set unit_status = 1
+where id = 10;
+INSERT INTO `ad_unit` (`plan_id`, `unit_name`, `unit_status`, `position_type`, `budget`, `create_time`, `update_time`)
+VALUES (10, '第二个推广单元', 1, 1, 15000000, '2018-01-01 00:00:00', '2018-01-01 00:00:00');
+delete
+from ad_unit
+where id = 11;
+
+
+-- 插入, 删除 creative_unit
+INSERT INTO `creative_unit` (`creative_id`, `unit_id`)
+VALUES (10, 12);
+delete
+from creative_unit
+where creative_id = 10
+  and unit_id = 12;
+
+
+-- 插入, 删除 ad_unit_district
+INSERT INTO `ad_unit_district` (`unit_id`, `province`, `city`)
+VALUES (10, '辽宁省', '大连市');
+delete
+from ad_unit_district
+where unit_id = 10
+  and province = '辽宁省'
+  and city = '大连市';
+
+-- 插入, 删除 ad_unit_it
+INSERT INTO `ad_unit_it` (`unit_id`, `it_tag`)
+VALUES (10, '徒步');
+delete
+from ad_unit_it
+where unit_id = 10
+  and it_tag = '徒步';
+
+-- 插入, 删除 ad_unit_keyword
+INSERT INTO `ad_unit_keyword` (`unit_id`, `keyword`)
+VALUES (10, '标志');
+delete
+from ad_unit_keyword
+where unit_id = 10
+  and keyword = '标志';
