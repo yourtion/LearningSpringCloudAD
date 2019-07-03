ed95b0fb8a9244dc161faab0d1adc48a2328dc00
YourtionGuo <yourtion@gmail.com>
2019-07-03 12:34:08 +0800

    feat(ad-search): 添加Level2第二层级操作

--- ad-service/ad-search/src/main/java/com/yourtion/springcloud/ad/handler/AdLevelDataHandler.java
+++ ad-service/ad-search/src/main/java/com/yourtion/springcloud/ad/handler/AdLevelDataHandler.java
@@ -1,8 +1,16 @@
 package com.yourtion.springcloud.ad.handler;
 
+import com.yourtion.springcloud.ad.dump.table.AdCreativeTable;
+import com.yourtion.springcloud.ad.dump.table.AdPlanTable;
+import com.yourtion.springcloud.ad.index.DataTable;
 import com.yourtion.springcloud.ad.index.IndexAware;
+import com.yourtion.springcloud.ad.index.adplan.AdPlanIndex;
+import com.yourtion.springcloud.ad.index.adplan.AdPlanObject;
+import com.yourtion.springcloud.ad.index.creative.CreativeIndex;
+import com.yourtion.springcloud.ad.index.creative.CreativeObject;
 import com.yourtion.springcloud.ad.mysql.constant.OpType;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 
 /**
  * 层级数据加载器
@@ -15,8 +23,33 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class AdLevelDataHandler {
 
-    private static <K, V> void handleBinlogEvent(IndexAware<K, V> index, K key, V value, OpType type) {
-        switch (type) {
+    public static void handleLevel2(AdPlanTable planTable, OpType op) {
+        val planObject = new AdPlanObject(
+                planTable.getId(),
+                planTable.getUserId(),
+                planTable.getPlanStatus(),
+                planTable.getStartDate(),
+                planTable.getEndDate()
+        );
+        handleBinlogEvent(DataTable.of(AdPlanIndex.class), planObject.getPlanId(), planObject, op);
+    }
+
+    public static void handleLevel2(AdCreativeTable creativeTable, OpType op) {
+        val creativeObject = new CreativeObject(
+                creativeTable.getAdId(),
+                creativeTable.getName(),
+                creativeTable.getType(),
+                creativeTable.getMaterialType(),
+                creativeTable.getHeight(),
+                creativeTable.getWidth(),
+                creativeTable.getAuditStatus(),
+                creativeTable.getAdUrl()
+        );
+        handleBinlogEvent(DataTable.of(CreativeIndex.class), creativeObject.getAdId(), creativeObject, op);
+    }
+
+    private static <K, V> void handleBinlogEvent(IndexAware<K, V> index, K key, V value, OpType op) {
+        switch (op) {
             case ADD:
                 index.add(key, value);
                 break;
