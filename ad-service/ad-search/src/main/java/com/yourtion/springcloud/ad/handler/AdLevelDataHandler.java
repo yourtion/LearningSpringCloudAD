0453f73dce8ee2159404847338ec008194e34f62
YourtionGuo <yourtion@gmail.com>
2019-07-03 13:00:47 +0800

    feat(ad-search): 添加Level4第四层级操作

--- ad-service/ad-search/src/main/java/com/yourtion/springcloud/ad/handler/AdLevelDataHandler.java
+++ ad-service/ad-search/src/main/java/com/yourtion/springcloud/ad/handler/AdLevelDataHandler.java
@@ -1,10 +1,7 @@
 package com.yourtion.springcloud.ad.handler;
 
 import com.alibaba.fastjson.JSON;
-import com.yourtion.springcloud.ad.dump.table.AdCreativeTable;
-import com.yourtion.springcloud.ad.dump.table.AdCreativeUnitTable;
-import com.yourtion.springcloud.ad.dump.table.AdPlanTable;
-import com.yourtion.springcloud.ad.dump.table.AdUnitTable;
+import com.yourtion.springcloud.ad.dump.table.*;
 import com.yourtion.springcloud.ad.index.DataTable;
 import com.yourtion.springcloud.ad.index.IndexAware;
 import com.yourtion.springcloud.ad.index.adplan.AdPlanIndex;
@@ -15,10 +12,16 @@ import com.yourtion.springcloud.ad.index.creative.CreativeIndex;
 import com.yourtion.springcloud.ad.index.creative.CreativeObject;
 import com.yourtion.springcloud.ad.index.creativeunit.CreativeUnitIndex;
 import com.yourtion.springcloud.ad.index.creativeunit.CreativeUnitObject;
+import com.yourtion.springcloud.ad.index.district.UnitDistrictIndex;
+import com.yourtion.springcloud.ad.index.interest.UnitItIndex;
+import com.yourtion.springcloud.ad.index.keyword.UnitKeywordIndex;
 import com.yourtion.springcloud.ad.mysql.constant.OpType;
 import com.yourtion.springcloud.ad.utils.CommonUtils;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+
+import java.util.Collections;
+import java.util.HashSet;
 
 /**
  * 层级数据加载器
@@ -95,6 +98,61 @@ public class AdLevelDataHandler {
         );
         val key = CommonUtils.stringConcat(creativeUnitObject.getAdId().toString(), creativeUnitObject.getUnitId().toString());
         handleBinlogEvent(DataTable.of(CreativeUnitIndex.class), key, creativeUnitObject, op);
+    }
+
+    public static void handleLevel4(AdUnitDistrictTable districtTable, OpType op) {
+        // 不支持更新
+        if (op == OpType.UPDATE) {
+            log.error("UnitDistrictIndex not support update");
+            return;
+        }
+
+        // 校验 AdUnit
+        val unitObject = DataTable.of(AdUnitIndex.class).get(districtTable.getUnitId());
+        if (null == unitObject) {
+            log.error("handleLevel4 found UnitDistrictIndex index error: {}", districtTable.getUnitId());
+            return;
+        }
+
+        val key = CommonUtils.stringConcat(districtTable.getProvince(), districtTable.getCity());
+        val value = new HashSet<Long>(Collections.singleton(districtTable.getUnitId()));
+        handleBinlogEvent(DataTable.of(UnitDistrictIndex.class), key, value, op);
+    }
+
+    public static void handleLevel4(AdUnitItTable itTable, OpType op) {
+        // 不支持更新
+        if (op == OpType.UPDATE) {
+            log.error("UnitItIndex not support update");
+            return;
+        }
+
+        // 校验 AdUnit
+        val unitObject = DataTable.of(AdUnitIndex.class).get(itTable.getUnitId());
+        if (null == unitObject) {
+            log.error("handleLevel4 found UnitItIndex index error: {}", itTable.getUnitId());
+            return;
+        }
+
+        val value = new HashSet<Long>(Collections.singleton(itTable.getUnitId()));
+        handleBinlogEvent(DataTable.of(UnitItIndex.class), itTable.getItTag(), value, op);
+    }
+
+    public static void handleLevel4(AdUnitKeywordTable keywordTable, OpType op) {
+        // 不支持更新
+        if (op == OpType.UPDATE) {
+            log.error("UnitKeywordIndex not support update");
+            return;
+        }
+
+        // 校验 AdUnit
+        val unitObject = DataTable.of(AdUnitIndex.class).get(keywordTable.getUnitId());
+        if (null == unitObject) {
+            log.error("handleLevel4 found UnitKeywordIndex index error: {}", keywordTable.getUnitId());
+            return;
+        }
+
+        val value = new HashSet<Long>(Collections.singleton(keywordTable.getUnitId()));
+        handleBinlogEvent(DataTable.of(UnitKeywordIndex.class), keywordTable.getKeyword(), value, op);
     }
 
     private static <K, V> void handleBinlogEvent(IndexAware<K, V> index, K key, V value, OpType op) {
