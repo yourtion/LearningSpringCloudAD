ea0790ca56b7c3700586c8cfa9dee8e4943c9845
YourtionGuo <yourtion@gmail.com>
2019-07-08 11:30:38 +0800

    feat(ad-search): AggregationListener完成binlog解析

--- ad-service/ad-search/src/main/java/com/yourtion/springcloud/ad/mysql/listener/AggregationListener.java
+++ ad-service/ad-search/src/main/java/com/yourtion/springcloud/ad/mysql/listener/AggregationListener.java
@@ -1,10 +1,7 @@
 package com.yourtion.springcloud.ad.mysql.listener;
 
 import com.github.shyiko.mysql.binlog.BinaryLogClient;
-import com.github.shyiko.mysql.binlog.event.Event;
-import com.github.shyiko.mysql.binlog.event.EventData;
-import com.github.shyiko.mysql.binlog.event.EventType;
-import com.github.shyiko.mysql.binlog.event.TableMapEventData;
+import com.github.shyiko.mysql.binlog.event.*;
 import com.yourtion.springcloud.ad.mysql.TemplateHolder;
 import com.yourtion.springcloud.ad.mysql.dto.BinlogRawData;
 import lombok.extern.slf4j.Slf4j;
@@ -12,9 +9,13 @@ import lombok.var;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.Serializable;
+import java.util.*;
+import java.util.stream.Collectors;
 
+/**
+ * @author yourtion
+ */
 @Slf4j
 @Component
 public class AggregationListener implements BinaryLogClient.EventListener {
@@ -89,8 +90,59 @@ public class AggregationListener implements BinaryLogClient.EventListener {
         }
     }
 
+    private List<Serializable[]> getAfterValues(EventData eventData) {
+        if (eventData instanceof WriteRowsEventData) {
+            return ((WriteRowsEventData) eventData).getRows();
+        }
+
+        if (eventData instanceof UpdateRowsEventData) {
+            return ((UpdateRowsEventData) eventData).getRows().stream()
+                    .map(Map.Entry::getValue)
+                    .collect(Collectors.toList());
+        }
+
+        if (eventData instanceof DeleteRowsEventData) {
+            return ((DeleteRowsEventData) eventData).getRows();
+        }
+
+        return Collections.emptyList();
+    }
+
     private BinlogRawData buildRawData(EventData data) {
-        // TODO: 完成方法
-        return null;
+        var table = templateHolder.getTable(tableName);
+        if (null == table) {
+            log.warn("table {} not found", table);
+            return null;
+        }
+
+        List<Map<String, String>> afterMapList = new ArrayList<>();
+        for (Serializable[] value : getAfterValues(data)) {
+            var colLen = value.length;
+
+            Map<String, String> afterMap = new HashMap<>(colLen);
+
+            for (int ix = 0; ix < colLen; ++ix) {
+
+                // 去除当前位置对应的列名
+                var colName = table.getPosMap().get(ix);
+
+                // 如果没有则说明不关心这个列
+                if (null == colName) {
+                    log.debug("ignore position: {}", ix);
+                    continue;
+                }
+
+                String colValue = value[ix].toString();
+                afterMap.put(colName, colValue);
+            }
+
+            afterMapList.add(afterMap);
+        }
+
+        var rawData = new BinlogRawData();
+        rawData.setAfter(afterMapList);
+        rawData.setTable(table);
+
+        return rawData;
     }
 }
