aab820f91589854870241ba906ea9c4936052765
YourtionGuo <yourtion@gmail.com>
2019-07-20 15:39:10 +0800

    feat(ad-search): 完善广告检索服务

--- ad-service/ad-search/src/main/java/com/yourtion/springcloud/ad/controller/SearchController.java
+++ ad-service/ad-search/src/main/java/com/yourtion/springcloud/ad/controller/SearchController.java
@@ -5,6 +5,9 @@ import com.yourtion.springcloud.ad.annotation.IgnoreResponseAdvice;
 import com.yourtion.springcloud.ad.client.SponsorClient;
 import com.yourtion.springcloud.ad.client.vo.AdPlan;
 import com.yourtion.springcloud.ad.client.vo.AdPlanGetRequest;
+import com.yourtion.springcloud.ad.search.ISearch;
+import com.yourtion.springcloud.ad.search.vo.SearchRequest;
+import com.yourtion.springcloud.ad.search.vo.SearchResponse;
 import com.yourtion.springcloud.ad.vo.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,13 +25,22 @@ import java.util.List;
 @RestController
 public class SearchController {
 
+    private final ISearch search;
+
     private final RestTemplate restTemplate;
     private final SponsorClient sponsorClient;
 
     @Autowired
-    public SearchController(RestTemplate restTemplate, SponsorClient sponsorClient) {
+    public SearchController(RestTemplate restTemplate, SponsorClient sponsorClient, ISearch search) {
         this.restTemplate = restTemplate;
         this.sponsorClient = sponsorClient;
+        this.search = search;
+    }
+
+    @PostMapping("/fetchAds")
+    public SearchResponse fetchAds(@RequestBody SearchRequest request) {
+        log.info("ad-search: fetchAds -> {}", JSON.toJSONString(request));
+        return search.fetchAds(request);
     }
 
     @IgnoreResponseAdvice
